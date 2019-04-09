136897c58b351665a2a5c02939ea790e6ef31849
David Leitner <david@leitner.io>
2019-04-10 01:44:30 +0200

    Add Bonus Solution

--- src/app/hello-world/hello-world.component.ts
+++ src/app/hello-world/hello-world.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit, Injectable, OnDestroy } from '@angular/core';
-import { Observable, Subscription, of } from 'rxjs';
+import {
+  Observable,
+  of,
+  interval,
+  merge,
+  combineLatest
+} from 'rxjs';
 import { map, catchError } from 'rxjs/operators';
 
 @Injectable({
@@ -9,7 +15,7 @@ export class DataProviderService {
   public loadGreeting(): Observable<string> {
     return new Observable<string>(observer => {
       setTimeout(() => observer.next('hello'), 5000);
-      setTimeout(() => observer.error('error'), 6000);
+      // setTimeout(() => observer.error('error'), 6000);
       setTimeout(() => observer.next('world!'), 15000);
       setTimeout(() => observer.complete(), 15001);
     });
@@ -28,9 +34,19 @@ export class HelloWorldComponent implements OnInit {
   public greeting$: Observable<string>;
 
   ngOnInit() {
-    this.greeting$ = this.dataProviderService.loadGreeting().pipe(
-      catchError(() => of('error')),
-      map(v => v.toUpperCase()),
+    const secondObservable = interval(1000);
+    const greetingObservable = merge(
+      of(''),
+      this.dataProviderService.loadGreeting().pipe(
+        catchError(() => of('error')),
+        map(v => v.toUpperCase())
+      )
+    );
+
+    this.greeting$ = combineLatest(secondObservable, greetingObservable).pipe(
+      map(value => {
+        return `${value[0]}s: ${value[1]}`;
+      })
     );
   }
 }
